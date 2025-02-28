bb72165552c82319f2c447762febc9f5b27b966f
Aurora2Link <aurora2link@gmail.com>
2025-02-28 10:40:36 -0600

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -2,14 +2,18 @@ from celery import Celery
 import os
 import redis
 import json
-import time
+import logging
+
+# Configurar el logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Configurar Redis
 REDIS_URL = os.getenv("REDIS_URL")
 redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
 # Configurar Celery
-celery = Celery("tasks", broker=REDIS_URL, backend=REDIS_URL)
+celery = Celery("celery_worker", broker=REDIS_URL, backend=REDIS_URL, include=['celery_worker'])
 
 @celery.task
 def process_message(data_str):
@@ -18,17 +22,18 @@ def process_message(data_str):
         data = json.loads(data_str)
         Phone_number = data['entry'][0]['changes'][0]['value']['messages'][0]['from']
         Message = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-        print(f"Mensaje recibido de {Phone_number}: {Message}")
+        logger.info(f"Mensaje recibido de {Phone_number}: {Message}")
         redis_client.lpush("processed_message_queue", f"{Phone_number}:{Message}")
-        print("Mensaje procesado y almacenado en Redis exitosamente.")
+        logger.info("Mensaje procesado y almacenado en Redis exitosamente.")
     except Exception as e:
-        print(f"Error al procesar el mensaje: {str(e)}")
+        logger.error(f"Error al procesar el mensaje: {str(e)}")
 
+@celery.task
 def fetch_and_process_messages():
     while True:
         _, data_str = redis_client.brpop("message_queue")
+        logger.info(f"Mensaje obtenido de Redis: {data_str}")
         process_message.delay(data_str)
-        time.sleep(1)  # Evitar sobrecargar el worker
 
 if __name__ == "__main__":
-    fetch_and_process_messages()
+    celery.worker_main(["worker", "--loglevel=info"])
